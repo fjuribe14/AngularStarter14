6101330de68ba1ba8b98d6e9692f5b37ba13764d
fjuribe14 <fjuribe.14@gmail.com>
2022-11-04 16:52:50 -0400

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+/**
+ * Components
+ */
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { LayoutComponent } from './components/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/Inicio' },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'Inicio',
+        component: InicioComponent,
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: '/Inicio',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+import { LayoutComponent } from './components/layout.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, LayoutComponent, InicioComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    SweetAlert2Module.forRoot(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
